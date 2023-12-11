8ebedd189d0382c35a06f76bb859edbc51d8c072
sheyla astorga <sheylaastorga1998@gmail.com>
2023-12-11 10:08:10 -0300

    instalamos la libreria de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 
 function App() {
   
 
   return (
-    <h1>Titulo de prueba</h1>
+    <h1 className='display-3'>Titulo de prueba</h1>
   )
 }
 
